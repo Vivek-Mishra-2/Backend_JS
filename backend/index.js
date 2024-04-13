8621116b2a534b8de7a019bd6c22fdb165fd7f44
Vivek Mishra <vivek.mishra@s.amity.edu>
2024-04-13 12:40:19 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+
+// import cors from 'cors'
+
+const app = express();
+// app.use(cors());
+
+
+// app.get('/', (req,  res) => {
+//     res.send('Server is ready');
+// });
+
+// get a list of 5 jokes
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'This is another joke'
+        },
+        {
+            id: 3,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 4,
+            title: 'Another joke',
+            content: 'This is another joke'
+        },
+        {
+            id: 5,
+            title: 'A joke',
+            content: 'This is a joke'
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Full Stack Development</h1>
+      <p>Jokes: {jokes.length}</p>
+
+      {
+        jokes.map((joke) => (
+            <div key={joke.id}>
+              <h3>{joke.title}</h3>
+              <p>{joke.content}</p>
+            </div>
+          ))
+      }
+    </>
+  )
+}
+
+export default App
